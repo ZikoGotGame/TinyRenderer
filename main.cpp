6a28038f9c1b0b0ade7841594184e183cf9fa272
Zac Tawfick <101304267+ZikoGotGame@users.noreply.github.com>
2025-07-13 16:33:56 -0400

    Created a very crappy wire mesh renderer.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "tgaimage.h"
+#include "wire_mesh-renderer.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -6,8 +7,9 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 int main(int argc, char **argv)
 {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	// image.set(52, 41, red);
+	// image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	WireMeshRenderer::draw_line(52, 41, 100, 100, image, white);
 	image.write_tga_file("output.tga");
 	return 0;
 }
---
+++ wire_mesh-renderer.h
@@ -0,0 +1,14 @@
+#include "tgaimage.h"
+
+struct WireMeshRenderer
+{
+    static void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+    {
+        for (float t = 0.; t < 1.; t += 0.1)
+        {
+            int x = x0 + (x1 - x0) * t;
+            int y = y0 + (y1 - y0) * t;
+            image.set(x, y, color);
+        }
+    }
+};
